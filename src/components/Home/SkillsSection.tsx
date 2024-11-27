80f44030b92507adef71d617f3182f881d665c8a
celina2304 <celina23042001@gmail.com>
2024-11-28 00:18:08 +0530

    updated skills section

--- src/components/Home/SkillsSection.tsx
+++ src/components/Home/SkillsSection.tsx
@@ -1,9 +1,61 @@
+import { useState } from "react";
 import { FunctionalComponentProps } from "../../types/components";
-import Button from "../ui/Button";
+// import Button from "../ui/Button";
 
 const skills = ["React.js", "Next.js", "Tailwind CSS", "Node.js", "Express.js"];
 
+const skillSet = [
+  {
+    skill: "React",
+    dark: "/images/skills/react.svg",
+    light: "/images/skills/react.svg",
+  },
+  {
+    skill: "CSS",
+    dark: "/images/skills/css.svg",
+    light: "/images/skills/css.svg",
+  },
+  {
+    skill: "Git",
+    dark: "/images/skills/git.svg",
+    light: "/images/skills/git.svg",
+  },
+  {
+    skill: "GitHub",
+    dark: "/images/skills/github.svg",
+    light: "/images/skills/github.svg",
+  },
+  {
+    skill: "HTML",
+    dark: "/images/skills/html.svg",
+    light: "/images/skills/html.svg",
+  },
+  {
+    skill: "Javascript",
+    dark: "/images/skills/javascript.svg",
+    light: "/images/skills/javascript.svg",
+  },
+  {
+    skill: "MongoDB",
+    dark: "/images/skills/mongodb.svg",
+    light: "/images/skills/mongodb.svg",
+  },
+  {
+    skill: "NodeJS",
+    dark: "/images/skills/nodejs.svg",
+    light: "/images/skills/nodejs.svg",
+  },
+  {
+    skill: "Tailwind Css",
+    dark: "https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500",
+    light:
+      "https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500",
+  },
+];
+
 const SkillsSection: React.FC<FunctionalComponentProps> = ({ id }) => {
+  const [activeSkill, setActiveSkill] = useState<number>(-1);
+
   return (
     <section
       id={id}
@@ -14,18 +66,18 @@ const SkillsSection: React.FC<FunctionalComponentProps> = ({ id }) => {
       </div>
       <br />
       <div className="flex flex-wrap gap-10">
-        {skills.map((skill, skillIndex) => {
+        {skillSet.map((skill, skillIndex) => {
           return (
-            <div
-              key={`skill-${skillIndex}-${skill}`}
-              className="hover:scale-[1.05] hover:transition-transform hover:ease-in-out duration-100 cursor-pointer hover:text-black border hover:bg-green_yellow border-green_yellow p-5 flex flex-wrap items-center justify-center md:text-2xl 2xl:text-tusker-card-heading font-semibold"
-            >
-              {skill}
-            </div>
+            <img
+              key={`${skillIndex}-skill-skills-section`}
+              src={skill.dark}
+              className="h-[100px] w-auto m-5"
+            />
+            // <img src={skill.light} className="h-[80px] w-auto bg-green-100" />
           );
         })}
       </div>
-      <div className="pt-10 flex items-center justify-center">
+      {/* <div className="pt-10 flex items-center justify-center">
         <Button
           onClick={() => {
             // navigate("/projects")
@@ -33,7 +85,7 @@ const SkillsSection: React.FC<FunctionalComponentProps> = ({ id }) => {
           type="button"
           label="View more"
         />
-      </div>
+      </div> */}
     </section>
   );
 };
